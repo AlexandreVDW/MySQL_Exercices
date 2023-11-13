bac2e27450acb85f210e64ca512f098728e8ebec
alexandreVDW <vandewielealexandre@gmail.com>
2023-11-13 15:07:45 +0100

    push

--- php-training-mysql/create.php
+++ php-training-mysql/create.php
@@ -1,3 +1,35 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Remplacez ces informations par les vôtres
+    $host = 'localhost';
+    $dbname = 'becode';
+    $user = 'root';
+    $pass = '';
+
+    try {
+        // Connexion à la base de données avec PDO
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+        // Préparation de la requête d'insertion
+        $query = $pdo->prepare('INSERT INTO hiking (name, difficulty, distance, duration, height_difference) VALUES (?, ?, ?, ?, ?)');
+
+        // Récupération des données du formulaire
+        $name = $_POST['name'];
+        $difficulty = $_POST['difficulty'];
+        $distance = $_POST['distance'];
+        $duration = $_POST['duration'];
+        $height_difference = $_POST['height_difference'];
+
+        // Exécution de la requête avec les valeurs du formulaire
+        $query->execute([$name, $difficulty, $distance, $duration, $height_difference]);
+
+        echo 'Randonnée ajoutée avec succès!';
+    } catch (PDOException $e) {
+        echo 'Erreur de connexion à la base de données : ' . $e->getMessage();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +38,7 @@
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
+	<a href="./read.php">Liste des données</a>
 	<h1>Ajouter</h1>
 	<form action="" method="post">
 		<div>
@@ -36,6 +68,13 @@
 		<div>
 			<label for="height_difference">Dénivelé</label>
 			<input type="text" name="height_difference" value="">
+		</div>
+		<div>
+			<label for="available">Disponibilité</label>
+			<select name="available">
+				<option value="1">Disponible</option>
+				<option value="0">Non disponible</option>
+			</select>
 		</div>
 		<button type="submit" name="button">Envoyer</button>
 	</form>
--- php-training-mysql/read.php
+++ php-training-mysql/read.php
@@ -7,8 +7,51 @@
   </head>
   <body>
     <h1>Liste des randonnées</h1>
-    <table>
-      <!-- Afficher la liste des randonnées -->
-    </table>
+    <?php
+      // Remplacez ces informations par les vôtres
+      $host = 'localhost';
+      $dbname = 'becode';
+      $user = 'root';
+      $pass = '';
+
+      try {
+          // Connexion à la base de données avec PDO
+          $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+          // Sélection des randonnées depuis la base de données
+          $query = $pdo->query('SELECT * FROM hiking');
+          $randonnees = $query->fetchAll(PDO::FETCH_ASSOC);
+
+          // Affichage des randonnées dans un tableau HTML
+          if (!empty($randonnees)) {
+            echo '<table border="1">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Difficulté</th>
+                        <th>Distance</th>
+                        <th>Durée</th>
+                        <th>Dénivelé</th>
+                    </tr>';
+  
+            foreach ($randonnees as $randonnee) {
+                echo '<tr>
+                        <td><a href="./update.php?id=' . $randonnee['id'] . '">' . $randonnee['name'] . '</a></td>
+                        <td>' . $randonnee['difficulty'] . '</td>
+                        <td>' . $randonnee['distance'] . 'km</td>
+                        <td>' . $randonnee['duration'] . '</td>
+                        <td>' . $randonnee['height_difference'] . 'm</td>
+                        <td>' . ($randonnee['available'] ? 'Disponible' : 'Non disponible') . '</td>
+                        <td><a href="./delete.php?id=' . $randonnee['id'] . '" onclick="return confirm(\'Êtes-vous sûr de vouloir supprimer cette randonnée ?\')">Supprimer</a></td>
+                      </tr>';
+            }
+  
+            echo '</table>';
+          } else {
+            echo 'Aucune randonnée trouvée.';
+          }
+      } catch (PDOException $e) {
+          echo 'Erreur de connexion à la base de données : ' . $e->getMessage();
+      }
+    ?>
   </body>
 </html>
